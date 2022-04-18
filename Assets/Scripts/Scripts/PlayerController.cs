ac5ac707d5489b7208e66d090588e6266d2ffbcb
Boredest <hodge195@gmail.com>
2022-04-18 15:37:20 -0700

    First commit

---
+++ Assets/Scripts/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(0, moveDirection * speed);
+    }
+}
