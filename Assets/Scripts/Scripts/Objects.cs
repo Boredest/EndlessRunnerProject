f589abcf8ad440bfcf8927f3d85dd5a0cc8cd922
Boredest <hodge195@gmail.com>
2022-05-15 22:17:42 -0700

    Tested random spawning code. Need to make it better and also adjust colliders. We also need to get two more sprites to add more variation to the project

---
+++ Assets/Scripts/Scripts/Objects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float leftBound;
+    public float offset = -1.0f;
+  
+    private void Start()
+    {
+        leftBound = Camera.main.ScreenToWorldPoint(Vector3.zero).x - offset;
+    }//Start
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if(transform.position.x < leftBound)
+        {
+            Destroy(gameObject);
+        }
+    }//Update
+}
---
+++ Assets/Scripts/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] objPrefab;
+    public float spawnPos;
+    public float offset = 5.0f;
+    int n;
+    public float spawnRate = 1f;
+    public float delayTime = 2.0f;
+
+    private float ySpawnPos = -3.5f;
+    public float xSpawnPos;
+ 
+
+    public void Awake()
+    {
+    
+    }//Awake
+    public void OnEnable()
+    {
+        n = Random.Range(0, objPrefab.Length);
+        InvokeRepeating(nameof(SpawnObj), spawnRate, delayTime);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(SpawnObj));
+    }
+
+    public void SpawnObj() {
+        //ranPos
+       // xSpawnPos = Random.Range(12, 15);
+       // transform.position = new Vector3(xSpawnPos,(float)-3.5, 0);
+  
+        Instantiate(objPrefab[n], transform.position, Quaternion.identity);
+      
+
+    }//SpawnObj
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
